cd0e32ca63b895dab8068ed00fb2fb6aa4a33211
dienigma <joshi.chinmay09@gmail.com>
2019-04-21 19:21:51 +0530

    Set up navbar and articles route.

---
+++ data.py
@@ -0,0 +1,18 @@
+def Articles():
+
+    articles = [
+        {
+            'id': 1,
+            "title": "Article One",
+            "body": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem error illo suscipit distinctio deserunt totam at expedita voluptatum ex! Voluptatem rerum delectus quo accusantium mollitia magni non sint exercitationem quasi.",
+            "author": "John Doe",
+            "create-date": "21-05-19"
+        },
+        {
+            'id': 2,
+            "title": "Article Two",
+            "body": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem error illo suscipit distinctio deserunt totam at expedita voluptatum ex! Voluptatem rerum delectus quo accusantium mollitia magni non sint exercitationem quasi.",
+            "author": "Chinmay Joshi",
+            "create-date": "23-05-19"
+        }
+    ]
